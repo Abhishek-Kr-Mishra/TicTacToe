07fd77fa93acd6f3e97979a3559064f05b1e84a4
abh224272 <abh224272@gmail.com>
2020-12-03 12:26:55 +0530

    Refactor: Spaces Between methods and Added comments as UC for every methods

--- TicTacToeGame.java
+++ TicTacToeGame.java
@@ -8,6 +8,7 @@ public class TicTacToeGame {
 	char[] gameBoard=new char[10];
 	char playerSymbol;
 	
+	//CreatingBoard_UC1
 	public char[] printBoard() {
 		for(int i=1; i<=9; i++) {
 			gameBoard[i]=' ';
@@ -15,20 +16,20 @@ public class TicTacToeGame {
 		return gameBoard;
 	}
 	
+	//ChooseACharacter_UC2 
 	public char chooseYourCharacter() {
 		System.out.println("Choose a Character X OR O ");
 		playerSymbol =sc.next().charAt(0);
 		return playerSymbol;
 	}
 	
+	//showBoard_UC3
 	public void showBoard(char[] gameBoard) {
 		int i=1;
 		while(i<=9) {
 			System.out.println(gameBoard[i]+"|"+gameBoard[i+1]+"|"+gameBoard[i+2]+"|");
 			i=i+3;
-		}
-		
+		}	
 	}
-		
 }
 
